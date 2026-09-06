7df1489f7023b6b1ee1862dc533bd53d7924ee96
yin753 <101307346+yin753@users.noreply.github.com>
2026-09-06 21:53:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,6 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+echo "CONFIG_USE_HOST_CMAKE=y" >> .config
+echo "CONFIG_USE_HOST_NINJA=y" >> .config
